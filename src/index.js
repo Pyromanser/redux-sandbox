7eeb583670b6645a34987212f14eb2e717a97ea8
Yaroslav <yaroslav.zhuvanov.ddi@gmail.com>
2020-10-01 13:30:37 +0300

    reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INK":
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, {type: "INK"});
+console.log(state);
+state = reducer(state, {type: "INK"});
+console.log(state);
