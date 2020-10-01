f7c4f6bffec2fbd8ccaefec05d701c2600b9eb7e
Yaroslav <yaroslav.zhuvanov.ddi@gmail.com>
2020-10-01 14:17:49 +0300

    UI for Redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import {createStore} from "redux";
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
-    case "INK":
+    case "INC":
       return state + 1;
+    case "DEC":
+      return state - 1;
     default:
       return state;
   }
@@ -11,9 +13,22 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+document
+  .getElementById("inc")
+  .addEventListener("click", () => {
+    store.dispatch({type: "INC"})
+  });
 
-store.dispatch({type:"INK"});
-store.dispatch({type:"INK"});
+document
+  .getElementById("dec")
+  .addEventListener("click", () => {
+    store.dispatch({type: "DEC"})
+  });
+
+const update = () => {
+  document
+    .getElementById("counter")
+    .innerText = store.getState();
+};
+
+store.subscribe(update);
